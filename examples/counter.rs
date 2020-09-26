22df46b1a107c50a96f710ca1083efdf116586e3
Alec Savvy <alecsavvy@outlook.com>
2020-09-26 14:57:20 -0700

    work on counter example

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,6 +1,48 @@
+#[macro_use]
+extern crate tokio_data;
+#[macro_use]
+extern crate tokio;
+
+#[derive(Debug)]
+pub struct Counter {
+    count: usize,
+}
+
+impl Counter {
+    pub fn new() -> Self {
+        Counter { count: 0 }
+    }
+    pub fn add(&mut self) {
+        self.count = self.count + 1;
+    }
+
+    pub fn get(&self) -> usize {
+        self.count
+    }
+}
+
+tokio_data!(Counter, Counter::new());
+
 #[tokio::main]
 async fn main() {
-    tokio_data::spawn(|name| async move {
-        println!("hello {}!", name);
+    let add_two = spawn_data(|data| async move {
+        let mut data = data.lock().await;
+        data.add();
+        data.add();
+        println!("counter at {:?}!", data.get());
+        data.get()
     });
+
+    let add_one = spawn_data(|data| async move {
+        tokio::time::delay_for(std::time::Duration::from_millis(10000)).await;
+        let mut data = data.lock().await;
+        data.add();
+        println!("counter at {:?}!", data.get());
+        data.get()
+    });
+
+    let (one, two) = join!(add_one, add_two);
+
+    println!("one: {}", one.await.unwrap());
+    println!("two: {}", two.await.unwrap())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,27 @@
 pub extern crate tokio;
 
-use std::sync::Arc;
-use tokio::sync::Mutex;
+#[macro_export]
+macro_rules! tokio_data {
+    ($data_type:ty, $data:expr) => {
+        type TokioData = $data_type;
 
-thread_local! {
-    static DATA: Arc<Mutex<String>> = Arc::new(Mutex::new("Alec".into()));
-}
+        use std::sync::Arc;
+        use tokio::sync::{Mutex, MutexGuard};
 
-use std::future::Future;
-use tokio::task::JoinHandle;
+        thread_local! {
+            static DATA: Arc<Mutex<TokioData>> = Arc::new(Mutex::new($data));
+        }
 
-pub fn spawn<T, F>(task: F) -> JoinHandle<T::Output>
-where
-    T: Future + Send + 'static,
-    T::Output: Send + 'static,
-    F: Fn(String) -> T + Send + 'static,
-{
-    tokio::spawn(task("alec".into()))
+        use std::future::Future;
+        use tokio::task::JoinHandle;
+        pub async fn spawn_data<T, F>(task: F) -> JoinHandle<T::Output>
+        where
+            T: Future + Send + 'static,
+            T::Output: Send + 'static,
+            F: Fn(Arc<Mutex<TokioData>>) -> T + Send,
+        {
+            let data_arc = DATA.with(|data| data.clone());
+            tokio::spawn(task(data_arc))
+        }
+    };
 }
